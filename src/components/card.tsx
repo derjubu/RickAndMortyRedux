cbf48c1f463a1e4e13d481f0701cb757c167feef
EdwardBock <edward.bock@palasthotel.de>
2022-01-13 18:02:05 +0100

    some suggestions to card.tsx
    - type definitions should start with camel case letter
    - useCallback to prevent performance issues
    - better naming of function names oToggleBookmark

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,26 +1,27 @@
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 import { toggle } from '../slices/Bookmarks/BookmarksSlice'
+import {useCallback} from "react";
 
-type cardProps = {
+type CardProps = {
   name: string
   image: string
   id: number
 }
 
-export function Card({ name, image, id }: cardProps): JSX.Element {
+export function Card({ name, image, id }: CardProps): JSX.Element {
   const dispatch = useAppDispatch()
   const bookmarks = useAppSelector((state) => state.bookmarks)
 
-  function handleClick(id: number) {
-    return dispatch(toggle(id))
-  }
+    const onToggleBookmark = useCallback(()=>{
+        dispatch(toggle(id));
+    }, [ id ]);
 
   return (
     <div>
       <p>{name}</p>
       <img src={image} />
       <p>Favorite? {bookmarks.includes(id) ? 'yes' : 'no'}</p>
-      <button onClick={(event) => handleClick(id)}>Click Here</button>
+      <button onClick={onToggleBookmark}>Click Here</button>
     </div>
   )
 }
