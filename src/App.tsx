66d17919fc6a2e12ee4393d3653207aaf8fbd2bc
EdwardBock <edward.bock@palasthotel.de>
2022-01-13 18:03:33 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Card} from './components/card'
+import {Card} from './components/Card'
 import {useGetRickAndMortyCharactersQuery} from './slices/RickAndMortyApi/RickAndMortyApiSlice'
 
 function App() {
